5dace96e362cb7e24ef2eea8459709bacee8f486
vratant11 <vratant2012061@gmail.com>
2022-10-01 00:12:32 +0530

    Corrected

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ const api = {
   key:"820879fe983a42a2df518f73d6b45038",
   base:"https://api.openweathermap.org/data/2.5/"
 }
-
+// corrected
 function App() 
 {
   const [query,setQuery] = useState('');
